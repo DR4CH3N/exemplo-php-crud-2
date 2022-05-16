59d3b2f4850ff93062e326bad2ce7dba6398b0d5
DR4CH3N <thehellc4t@gmail.com>
2022-05-16 10:35:25 -0300

    adição dos nomes e IDs dos fabricantes

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -5,18 +5,24 @@
     $senha = "";
     $banco = "vendas";
 
-    // criando a conexão com o MySQL (API/Driver de conexão)
-    $conexao = new PDO(
-        "mysql:host=$servidor; dbname=$banco; charset=utf8",
-        $usuario,
-        $senha
-    );
+    // try/catch
 
-    // habilita a verificação de erros
-    $conexao->setAttribute(
+    try {
+        $conexao = new PDO( // PDO é uma API
+            "mysql:host=$servidor; dbname=$banco; charset=utf8",
+            $usuario,
+            $senha);
+
+         // habilita a verificação de erros
+        $conexao->setAttribute(
         PDO::ATTR_ERRMODE, // constante de erros em geral 
-        PDO::ERRMODE_EXCEPTION // constante de exceções de erro
-    );
+        PDO::ERRMODE_EXCEPTION); // constante de exceções de erro
+    
+    } catch (Exception $erro){
+        die("erro: " .$erro->getMessage()); // die vai mostrar uma mensagem personalizada e vai parar o programa completamente.
+        // getline vai mostrar a linha em que esta dando erro no codigo
+        
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -41,6 +47,31 @@
                 </tr>
             </thead>
             <tbody>
+        <?php 
+        // string com o comando SQL
+        $sql = "SELECT id, nome FROM fabricantes";
+
+        // preparação do comando
+        $consulta = $conexao->prepare($sql);
+
+        // execução do comando
+        $consulta->execute();
+
+        // capturar os resultados
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+     // echo"<pre>";
+     // var_dump($resultado);
+     // echo"</pre>";
+
+     foreach ($resultado as $fabricante) {
+         echo $fabricante['nome'];
+     }
+        ?>
+        <tr>
+            <td><?php echo $fabricante['id'];?></td>
+            <td><?php echo $fabricante['nome'];?></td>
+        </tr>
                 
             </tbody>
         </table>
