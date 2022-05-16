209ec02a659851263798dff811b98e496b375b04
DR4CH3N <thehellc4t@gmail.com>
2022-05-16 08:40:46 -0300

    criação de index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD vendas</title>
+    link
+</head>
+<body>
+    <div class="container">
+        <h1>CRUD - sistema vendas</h1>
+        <hr>
+        <p>
+            operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.
+        </p>
+
+        <ul>
+            <li><a href="fabricantes/listar.php">Fabricantes</a></li>
+            <li><a href="">Produtos</a></li>
+        </ul>
+    </div>
+</body>
+</html>
