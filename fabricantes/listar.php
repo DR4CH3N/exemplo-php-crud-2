57c3bcbb44ce58d02d64713f387354e58cd3b572
DR4CH3N <thehellc4t@gmail.com>
2022-05-16 09:10:43 -0300

    adicionado comandos de adição do PDO

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -1,3 +1,23 @@
+<?php 
+    // SCRIPT DE CONEXAO AO SERVIDOR BANCO DE DADOS
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "vendas";
+
+    // criando a conexão com o MySQL (API/Driver de conexão)
+    $conexao = new PDO(
+        "mysql:host=$servidor; dbname=$banco; charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    // habilita a verificação de erros
+    $conexao->setAttribute(
+        PDO::ATTR_ERRMODE, // constante de erros em geral 
+        PDO::ERRMODE_EXCEPTION // constante de exceções de erro
+    );
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,7 +31,19 @@
         <h1>Fabricantes | SELECT</h1>
         <hr>
         <h2>Lendo e carregando todos os fabricantes</h2>
-        <h2></h2>
+        
+        <table>
+            <caption>Lista de fabricantes</caption>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
     </div>
     
 </body>
