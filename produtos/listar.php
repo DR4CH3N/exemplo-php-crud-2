ffdd4be8b7d61da4899e423b02f76f7aa03dc2bf
DR4CH3N <thehellc4t@gmail.com>
2022-05-25 08:25:51 -0300

    adicionado listar produtos

---
+++ produtos/listar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - lista</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Produtos | SELECT</h1>
+        <hr>
+        <h2>Lendo e carregando todos os Produtos</h2>
+        
+        <p>
+            <a href="inserir.php">Inserir um novo Produto</a>
+
+        </p>
+    </div>
+        
+
+        
+        
+    
+</body>
+</html>
