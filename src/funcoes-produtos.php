223ef1a11b6fdd69aeb5ee4d739888bd44457613
DR4CH3N <thehellc4t@gmail.com>
2022-05-27 09:48:49 -0300

    inserir produtos funcionando

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -3,6 +3,21 @@ require_once "conecta.php";
 
 function lerProdutos(PDO $conexao):array {
     $sql = "SELECT produtos.id, produtos.nome AS produto, produtos.preco, produtos.descricao, produtos.quantidade, fabricantes.nome AS fabricante FROM produtos INNER JOIN fabricantes ON produtos.fabricante_id = fabricantes.id ORDER BY produto";
+
+    // try vai tentar todos esses comandos
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        // PDO fetch assoc vai retornar o resultado em array
+    }
+    // catch vai executar os comandos dentro dele caso algum comando do try não de certo
+    // die vai fazer com que o programa pare caso algum erro ocorra
+    catch (Exception $erro) {
+        die ("Erro: " .$erro->getMessage());
+    }
+    return $resultado;
+}
     
     function inserirProduto(PDO $conexao, string $nome, 
         float $preco, int $quantidade, string $descricao, int $fabricanteId):void
@@ -29,20 +44,8 @@ function lerProdutos(PDO $conexao):array {
         
     }
 
-    // try vai tentar todos esses comandos
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->execute();
-        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-        // PDO fetch assoc vai retornar o resultado em array
-    }
-    // catch vai executar os comandos dentro dele caso algum comando do try não de certo
-    // die vai fazer com que o programa pare caso algum erro ocorra
-    catch (Exception $erro) {
-        die ("Erro: " .$erro->getMessage());
-    }
-    return $resultado;
-}
+    
+
 
 // funções utilitarias
 
