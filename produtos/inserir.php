9b797b61b433c80b881b3c36d084831d1dbb8f6b
DR4CH3N <thehellc4t@gmail.com>
2022-05-27 09:21:33 -0300

    inserir produto adicionado

--- produtos/inserir.php
+++ produtos/inserir.php
@@ -1,3 +1,20 @@
+<?php
+require_once '../src/funcoes-fabricantes.php';
+$listaDeFabricantes = LerFabricantes($conexao);
+
+if(isset($_POST['inserir'])){ // isset verifica se algum recurso é ativado, caso sim ele vai executar o codigo especifico
+    require_once '../src/funcoes-produtos.php';
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+    $preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_NUMBER_FLOAT);
+    $quantidade = filter_input(INPUT_POST, 'quantidade', FILTER_SANITIZE_NUMBER_INT);
+    $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_SPECIAL_CHARS);
+    $fabricanteId = filter_input(INPUT_POST, 'fabricante', FILTER_SANITIZE_NUMBER_INT);
+
+    inserirProduto($conexao, $nome, $preco, $quantidade, $descricao, $fabricanteId);
+
+    header("location:listar.php");
+}
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -36,6 +53,14 @@
             <label for="fabricante">Fabricante</label>
             <select name="fabricante" id="fabricante">
                 <option value=""></option>
+                <?php foreach($listaDeFabricantes as $fabricante) { ?>
+                    <!-- o value ID é para o banco -->
+                    <option value="<?=$fabricante['id'] ?>">
+                        <?= $fabricante['nome'] ?> <!-- Exibição apenas -->
+                    </option>
+                <?php 
+                }    
+                ?>
                 <!-- opções de fabricantes existentes no BANCO -->
             </select>
         </p>
--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -4,6 +4,30 @@ require_once "conecta.php";
 function lerProdutos(PDO $conexao):array {
     $sql = "SELECT produtos.id, produtos.nome AS produto, produtos.preco, produtos.descricao, produtos.quantidade, fabricantes.nome AS fabricante FROM produtos INNER JOIN fabricantes ON produtos.fabricante_id = fabricantes.id ORDER BY produto";
     
+    function inserirProduto(PDO $conexao, string $nome, 
+        float $preco, int $quantidade, string $descricao, int $fabricanteId):void
+        { // void indica sem retorno
+        $sql = "INSERT INTO produtos(nome, preco, quantidade, descricao, fabricante_id) 
+        VALUES(:nome, :preco, :quantidade, :descricao, :fabricante_id)";       
+
+        try {
+            $consulta = $conexao->prepare($sql);
+            $consulta->bindParam(':nome', $nome, PDO::PARAM_STR);
+            $consulta->bindParam(':preco', $preco, PDO::PARAM_STR);
+            $consulta->bindParam(':quantidade', $quantidade, PDO::PARAM_INT);
+            $consulta->bindParam(':descricao', $descricao, PDO::PARAM_STR);
+            $consulta->bindParam(':fabricante_id', $fabricanteId, PDO::PARAM_INT);
+            $consulta->execute();
+
+            // PDO fetch assoc vai retornar o resultado em array
+        }
+        // catch vai executar os comandos dentro dele caso algum comando do try não de certo
+        // die vai fazer com que o programa pare caso algum erro ocorra
+        catch (Exception $erro) {
+            die ("Erro: " .$erro->getMessage());
+        }
+        
+    }
 
     // try vai tentar todos esses comandos
     try {
